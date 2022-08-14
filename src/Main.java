0a19c56ec996c8a1d9b3aad38daffdf9bc0e21d7
yanap <yanapenner96@gmail.com>
2022-08-14 15:34:03 +0300

    Домашняя работа по теме 1.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,88 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // Задание 1
+        byte clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+        // Задание 2
+        int clientDeviceYear = 2010;
+        if (clientDeviceYear < 2015) {
+            System.out.print("Установите облегченную версию приложения для ");
+        } else {
+            System.out.print("Установите версию приложения для ");
+        }
+        if (clientOS == 1) {
+            System.out.println("Android по ссылке");
+        } else {
+            System.out.println("iOS по ссылке");
+        }
+
+        // Задание 3
+        int year = 2022;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        // Задание 4
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        int interval = 40;
+        int startInterval = 20;
+        if (deliveryDistance <= startInterval) {
+            deliveryDays = 1;
+        } else {
+            deliveryDays = deliveryDays + (int) Math.ceil((deliveryDistance - startInterval) / (double) interval);
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
+
+        // Задание 5
+        int monthNumber = 8;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+            default:
+                System.out.println("Такого месяца в году не существует");
+
+        }
     }
 }
